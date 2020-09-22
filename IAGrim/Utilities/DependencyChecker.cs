3ee490576dda2b48a386e2035cba8ccae7f0d4f5
Marius <->
2020-09-22 11:56:54 -0300

    Upgraded SQLite library version

--- IAGrim/Utilities/DependencyChecker.cs
+++ IAGrim/Utilities/DependencyChecker.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -52,7 +53,7 @@ namespace IAGrim.Utilities {
             return false;
         }
 
-        // TODO: What uses this?
+        // TODO: What uses this? Anything?
         public static bool CheckVs2013Installed() {
             Logger.Debug("Checking if VC++ 2013 Runtimes are installed");
             using (RegistryKey registryKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Classes\Installer\Dependencies\{f65db027-aff3-4070-886a-0d87064aabb1}")) {
@@ -68,7 +69,7 @@ namespace IAGrim.Utilities {
             }
         }
 
-        // TODO: What uses this?
+        // TODO: What uses this? Anything?
         public static bool CheckVs2010Installed() {
             Logger.Debug("Checking if VC++ 2010 Runtimes are installed");
             using (RegistryKey registryKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Classes\Installer\Products\1D5E3C0FEDA1E123187686FED06E995A")) {
--- create_beta.bat
+++ create_beta.bat
@@ -1,8 +1,7 @@
-copy inno\vcredist_x64.exe IAGrim\bin\release\
 copy inno\vcredist_x86.exe IAGrim\bin\release\
 copy inno\NDP461-KB3102438-Web.exe IAGrim\bin\release\
 copy inno\2010sp1_vcredist_x86.exe IAGrim\bin\release\
-
+copy inno\2015rc3_vc_redist.x86.exe IAGrim\bin\release\
 
 
 copy IAGrim\bin\release\IAGrim.exe Installer\IAGrim_beta.exe
