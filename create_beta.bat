9dcd05ac5e9113e87cdcaa22a63e0371d991488a
Marius <->
2020-12-07 15:17:17 -0300

    Once again including the cefsharp dll in all builds.
    Was just a terrible idea excluding it, anyone updating from a >14 day old version would end up with a corrupted install, since CEFSharp was updated 14 days ago.

--- create_beta.bat
+++ create_beta.bat
@@ -1,23 +1,6 @@
-@echo off
-certutil -hashfile IAGrim\bin\Release\libcef.dll MD5 > current-cefsharp-hash.txt
-fc installer\expected-cefsharp.hash current-cefsharp-hash.txt > nul
-if errorlevel 1 goto error
-
-
-
-
 copy /y IAGrim\bin\release\IAGrim.exe Installer\IAGrim_beta.exe
 copy /y %appdata%\..\local\evilsoft\iagd\tags_ia.template.txt installer\tags_ia.txt
 
 Inno\iscc Inno\gdia_beta.iss
 
 echo All good
-goto end
-
-:error
-echo Did someone forgot this cannot be used after updating libcef?
-pause
-
-
-:end
-del /q current-cefsharp-hash.txt
