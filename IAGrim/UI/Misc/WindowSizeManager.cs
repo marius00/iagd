ba2aaffdf7336385a6df6f9e5385cfe2e9d647e3
m <m@example.com>
2021-10-29 06:51:44 +0200

    IA will now attempted to remember which monitor to launch on

--- IAGrim/UI/Misc/WindowSizeManager.cs
+++ IAGrim/UI/Misc/WindowSizeManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+using CefSharp.DevTools.HeadlessExperimental;
 using IAGrim.Settings;
 namespace IAGrim.UI.Misc {
     /// <summary>
@@ -15,22 +17,44 @@ namespace IAGrim.UI.Misc {
             _settingsService = settingsService;
             _form.FormClosing += _form_FormClosing;
 
-            var obj = _settingsService.GetLocal().WindowPositionSettings;
-            if (obj?.State != null && !_settingsService.GetLocal().StartMinimized) {
-                _form.WindowState = (FormWindowState)obj.State;
-            }
+            RestoreWindowPosition();
+        }
 
-            if (obj?.Width != null && obj?.Height != null) {
+        private void RestoreWindowPosition() {
+            var obj = _settingsService.GetLocal().WindowPositionSettings;
+            if (obj == null) return;
+
+
+            if (!_settingsService.GetLocal().StartMinimized) {
+                var screen = Screen.AllScreens.FirstOrDefault(scr => scr.DeviceName == obj.Display);
+                if (screen != null) {
+                    _form.StartPosition = FormStartPosition.Manual;
+                    var bounds = screen.Bounds;
+
+                    var top = obj.Top != null ? Math.Max(0, Math.Min(obj.Top.Value, Screen.FromControl(_form).Bounds.Height - 100)) : _form.Top;
+                    var left = obj.Left != null ? Math.Max(0, Math.Min(obj.Left.Value, Screen.FromControl(_form).Bounds.Width - 100)) : _form.Left;
+                    var width = obj.Width ?? _form.Width;
+                    var height = obj.Height ?? _form.Height;
+                    _form.SetBounds(bounds.X + left, bounds.Y + top, width, height);
+
+                    _form.WindowState = (FormWindowState)obj.State;
+                    return;
+
+                }
+
+                _form.WindowState = (FormWindowState) obj.State;
+            }
+                
+            if (obj.Width != null && obj.Height != null) {
                 _form.Size = new Size(obj.Width.Value, obj.Height.Value);
             }
 
-
-            if (obj?.Top != null) {
+            if (obj.Top != null) {
                 var top = Math.Max(0, Math.Min(obj.Top.Value, Screen.FromControl(_form).Bounds.Height - 100));
                 _form.Top = top;
             }
 
-            if (obj?.Left!= null) {
+            if (obj.Left != null) {
                 var left = Math.Max(0, Math.Min(obj.Left.Value, Screen.FromControl(_form).Bounds.Width - 100));
                 _form.Left = left;
             }
@@ -38,18 +62,20 @@ namespace IAGrim.UI.Misc {
 
         private void _form_FormClosing(object sender, FormClosingEventArgs e) {
             if (_form != null) {
+                var screenInfo = Screen.FromControl(_form);
                 var state = _form.WindowState == FormWindowState.Maximized ? FormWindowState.Maximized : FormWindowState.Normal;
                 var height = Math.Max(_form.Size.Height, 100);
                 var width = Math.Max(_form.Size.Width, 100);
-                var top = Math.Max(0, Math.Min(_form.Top, Screen.FromControl(_form).Bounds.Height - 100));
-                var left = Math.Max(0, Math.Min(_form.Left, Screen.FromControl(_form).Bounds.Width - 100));
+                var top = Math.Max(0, Math.Min(_form.Top, screenInfo.Bounds.Height - 100));
+                var left = Math.Max(0, Math.Min(_form.Left, screenInfo.Bounds.Width - 100));
 
                 var props = new WindowSizeProps {
                     State = (int)state,
                     Top = top,
                     Left = left,
                     Width = width,
-                    Height = height
+                    Height = height,
+                    Display = screenInfo.DeviceName
                 };
 
                 _settingsService.GetLocal().WindowPositionSettings = props;
@@ -63,6 +89,7 @@ namespace IAGrim.UI.Misc {
             public int? Width { get; set; }
             public int? Top { get; set; }
             public int? Left { get; set; }
+            public string Display { get; set; }
         }
         
     }
