6e914b3b933c8f0ce824269922483283e2aaa1bf
m@m.com <m@m.com>
2023-10-07 18:22:58 +0200

    x

--- WebUI/src/components/NotificationComponent.tsx
+++ WebUI/src/components/NotificationComponent.tsx
@@ -27,9 +27,9 @@ class NotificationContainer extends PureComponent<NotificationProps, object> {
     if (type === 'default')
       return <svg className={styles.icon +  " " +  styles.success} d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>;
     if (type === 'warning')
-      return <svg color={"secondary"} className={styles.icon} d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>;
+      return <svg  className={styles.icon} d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>;
     if (type === 'danger')
-      return <svg color={"secondary"} className={styles.icon} d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>;
+      return <svg className={styles.icon} d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>;
     return "Bug!";
   }
 
