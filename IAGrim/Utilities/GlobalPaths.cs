21c2a71c2b7e0926b9ccf4f6ecc1fd52068aaada
m@m.com <m@m.com>
2023-10-07 21:11:27 +0200

    Fixed a path typo for itemqueue

--- IAGrim/Utilities/GlobalPaths.cs
+++ IAGrim/Utilities/GlobalPaths.cs
@@ -38,7 +38,7 @@ namespace IAGrim.Utilities {
 
         public static string CsvLocation {
             get {
-                string path = Path.Combine(CoreFolder, "itemqueue", "ingoing");
+                string path = Path.Combine(CoreFolder, "itemqueue");
                 Directory.CreateDirectory(path);
                 return path;
             }
