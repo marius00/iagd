c5b59ea4b3856758b5b683153d6810f3663f9757
- <github@evilsoft.net>
2024-05-05 11:25:26 +0200

    Now uploads a dummy version file to grimdawn.evilsoft.net upon build, this will be used to link up towards github

--- getver.cmd
+++ getver.cmd
@@ -1,3 +1,5 @@
 @powershell "(Get-Item -path "iagrim\bin\release\iagrim.exe").VersionInfo.ProductVersion" > ver.txt
 @set /p IA_VERSION=<ver.txt
+@echo 1 > Installer\dummy-%IA_VERSION%
+@scp Installer\dummy-%IA_VERSION% storage@grimdawn.evilsoft.net:/home/storage/grimdawn.evilsoft.net/
 @del ver.txt
