576476391d56f5a6a174a7feb089faf13b248109
m <m@example.com>
2025-10-04 10:08:43 -0300

    Fixed an issue tagging IAGD versions

--- getver.cmd
+++ getver.cmd
@@ -1,4 +1,4 @@
-@powershell "(Get-Item -path "iagrim\bin\release\iagrim.exe").VersionInfo.ProductVersion" > ver.txt
+@powershell "(Get-Item -path "iagrim\bin\release\net9.0-windows7.0\iagrim.exe").VersionInfo.FileVersion" > ver.txt
 @set /p IA_VERSION=<ver.txt
 @type nul > Installer\dummy-%IA_VERSION%
 @scp Installer\dummy-%IA_VERSION% storage@grimdawn.evilsoft.net:/home/storage/grimdawn.evilsoft.net/
