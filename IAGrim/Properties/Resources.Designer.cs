4c020826494a1671c44fbfd0c8aa86619690902c
m <m@example.com>
2026-01-11 05:21:49 -0300

    The patreon/paypal button in the donate nag screen are now images, not backgrounds.
    See issue https://github.com/dotnet/winforms/issues/13720
    Bug in winforms on .net 10+

--- IAGrim/Properties/Resources.Designer.cs
+++ IAGrim/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace IAGrim.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -86,6 +86,16 @@ namespace IAGrim.Properties {
         internal static System.Drawing.Bitmap donate {
             get {
                 object obj = ResourceManager.GetObject("donate", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap donate1 {
+            get {
+                object obj = ResourceManager.GetObject("donate1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
