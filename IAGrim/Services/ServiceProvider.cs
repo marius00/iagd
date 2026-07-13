96b530c4c4d72175e6bbd17bd12d1db5ccba0692
m <m@m.m>
2026-07-13 12:46:18 -0300

    Fixed a slowdown issue when having an existing -WAL

--- IAGrim/Services/ServiceProvider.cs
+++ IAGrim/Services/ServiceProvider.cs
@@ -37,11 +37,19 @@ namespace IAGrim.Services {
 
         public static ServiceProvider Initialize() {
             Logger.Debug("Creating services");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            void Timed(string step) {
+                Logger.Info($"[timing] Init.{step} took {sw.ElapsedMilliseconds} ms");
+                sw.Restart();
+            }
+
             var factory = new SessionFactory();
 
             // Settings should be upgraded early, it contains the language pack etc and some services depends on settings.
             var settingsService = StartupService.LoadSettingsService();
+            Timed("LoadSettingsService");
             var grimDawnDetector = new GrimDawnDetector(settingsService);
+            Timed("new GrimDawnDetector");
 
             IPlayerItemDao playerItemDao;
             IDatabaseItemDao databaseItemDao;
@@ -62,6 +70,7 @@ namespace IAGrim.Services {
             itemSkillDao = new ItemSkillDaoImpl(factory);
             itemCollectionRepo = new ItemCollectionDaoImpl(factory);
             replicaItemDao = new ReplicaItemDaoImpl(factory);
+            Timed("Construct DAOs");
 
             
 
@@ -92,6 +101,7 @@ namespace IAGrim.Services {
             ];
 
             var cacher = new TransferStashServiceCache(databaseItemDao);
+            Timed("TransferStashServiceCache");
             services.Add(cacher);
 
 
