3474c67328992edf674ec13a01c5817cc31d88cf
m <m@m.m>
2026-07-29 05:22:38 -0300

    Fixed another zero-pad issue with versioning

--- set-commit-tags.cmd
+++ set-commit-tags.cmd
@@ -1,4 +1,6 @@
-@powershell "(Get-Item -path "iagrim\bin\release\net10.0-windows\win-x64\iagrim.exe").VersionInfo.FileVersion" > ver.txt
+@rem FileVersion is a numeric win32 resource and can't carry the zero-padded revision,
+@rem so read ProductVersion (AssemblyInformationalVersion) and strip the +commitsha suffix.
+@powershell -NoProfile -Command "(Get-Item -path 'iagrim\bin\release\net10.0-windows\win-x64\iagrim.exe').VersionInfo.ProductVersion.Split('+')[0]" > ver.txt
 set /p IA_VERSION=<ver.txt
 del ver.txt
 
